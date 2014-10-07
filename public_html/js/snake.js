0b8f9ce082de56c9f8864075e45fa078a0750117
Bahrudin Hacimic <bhacimic10@yahoo.com>
2014-10-07 15:51:43 -0700

    Work in progress for our snake project.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
+var snakeLength;
+var snakeSize;
 
+var context;
+var screenWidth;
+var screenHeight;
 
+gameIntialize();
+gameDraw();
+
+function gameIntialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeigh;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(242, 200, 12)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+    
+}
